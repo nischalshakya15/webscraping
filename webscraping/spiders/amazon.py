959e766e3494fd90cf941c689292b19520f9089a
nischalshakya15 <nischalshakya95@gmail.com>
2021-06-24 00:12:17 +0545

    Update amazon code

--- webscraping/spiders/amazon.py
+++ webscraping/spiders/amazon.py
@@ -14,16 +14,15 @@ class AmazonSpider(scrapy.Spider):
         print(amazon_category_top)
 
         for act in amazon_category_top:
-            print(act)
             category = act.xpath(
                 '//div[@class = "a-cardui-header"]/h2[@class = "a-color-base headline truncate-2line"]/text()'
             ).extract()
+            print(category)
 
             link = act.xpath(
-                '//div[@class = "a-cardui-footer"]/a/@href'
+                '//div[@class = "a-cardui-footer"]/a[@class = "a-link-normal see-more truncate-1line"]/@href'
             ).extract()
 
-            print(category)
             print(link)
 
             result = zip(category, link)
